ea5cc4b4b085869c142e914731d14d78462740ad
Gianguido Sorà <me@gsora.xyz>
2021-04-06 17:42:10 +0200

    ibc connection support

---
+++ gaia_processor/ibc.go
@@ -0,0 +1,99 @@
+package gaia_processor
+
+import (
+	"bytes"
+	"strings"
+
+	"github.com/cosmos/cosmos-sdk/x/ibc/core/03-connection/types"
+
+	host "github.com/cosmos/cosmos-sdk/x/ibc/core/24-host"
+	"go.uber.org/zap"
+
+	"github.com/allinbits/tracelistener"
+)
+
+type connectionWritebackPacket struct {
+	ConnectionID        string `db:"connection_id" json:"connection_id"`
+	ClientID            string `db:"client_id" json:"client_id"`
+	State               string `db:"state" json:"state"`
+	CounterConnectionID string `db:"counter_connection_id" json:"counter_connection_id"`
+	CounterClientID     string `db:"counter_client_id" json:"counter_client_id"`
+}
+
+type connectionCacheEntry struct {
+	connectionID string
+	clientID     string
+}
+
+var observedKeys = [][]byte{
+	[]byte(host.KeyConnectionPrefix),
+}
+
+type ibcProcessor struct {
+	l                *zap.SugaredLogger
+	connectionsCache map[connectionCacheEntry]connectionWritebackPacket
+}
+
+func (b *ibcProcessor) ModuleName() string {
+	return "ibc"
+}
+
+func (b *ibcProcessor) FlushCache() tracelistener.WritebackOp {
+	if len(b.connectionsCache) == 0 {
+		return tracelistener.WritebackOp{}
+	}
+
+	l := make([]interface{}, 0, len(b.connectionsCache))
+
+	for _, c := range b.connectionsCache {
+		l = append(l, c)
+	}
+
+	b.connectionsCache = map[connectionCacheEntry]connectionWritebackPacket{}
+
+	return tracelistener.WritebackOp{
+		DatabaseExec: insertConnection,
+		Data:         l,
+	}
+}
+
+func (b *ibcProcessor) OwnsKey(key []byte) bool {
+	for _, k := range observedKeys {
+		if bytes.HasPrefix(key, k) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func (b *ibcProcessor) Process(data tracelistener.TraceOperation) error {
+	keyFields := strings.FieldsFunc(string(data.Key), func(r rune) bool {
+		return r == '/'
+	})
+
+	b.l.Debugw("store key", "fields", keyFields, "raw key", string(data.Key))
+
+	// IBC keys are mostly strings
+	switch len(keyFields) {
+	case 2:
+		if keyFields[0] == host.KeyConnectionPrefix { // this is a ConnectionEnd
+			ce := types.ConnectionEnd{}
+			p.cdc.MustUnmarshalBinaryBare(data.Value, &ce)
+			b.l.Debugw("connection end", "data", ce)
+
+			b.connectionsCache[connectionCacheEntry{
+				connectionID: keyFields[1],
+				clientID:     ce.ClientId,
+			}] = connectionWritebackPacket{
+				ConnectionID:        keyFields[1],
+				ClientID:            ce.ClientId,
+				State:               ce.State.String(),
+				CounterConnectionID: ce.Counterparty.ConnectionId,
+				CounterClientID:     ce.Counterparty.ClientId,
+			}
+		}
+	}
+
+	return nil
+}
