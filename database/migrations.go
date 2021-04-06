f7c64945047b6c8bb1cf5a5fd626a453a0e83ad2
Gianguido Sorà <me@gsora.xyz>
2021-04-06 11:33:57 +0200

    move migrations out, provide way of registering migrations to be executed

--- database/migrations.go
+++ database/migrations.go
@@ -4,23 +4,16 @@ const createDatabase = `
 CREATE DATABASE IF NOT EXISTS tracelistener;
 `
 
-const createBalancesTable = `
-CREATE TABLE IF NOT EXISTS tracelistener.balances (
-	address text not null,
-	amount integer not null,
-	denom text not null,
-	height integer not null,
-	primary key (address)
-)
-`
-
 var migrationList = []string{
 	createDatabase,
-	createBalancesTable,
 }
 
 func (i *Instance) runMigrations() {
 	for _, m := range migrationList {
 		i.d.MustExec(m)
 	}
+}
+
+func RegisterMigration(migration ...string) {
+	migrationList = append(migrationList, migration...)
 }
