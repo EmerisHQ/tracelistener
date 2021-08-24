181788ac77030c5a01680b69a955f1ea565f8aa6
Gianguido Sorà <me@gsora.xyz>
2021-08-24 14:03:50 +0200

    api: try avoiding extra allocs on middlewares

--- utils/logging/rest.go
+++ utils/logging/rest.go
@@ -7,24 +7,29 @@ import (
 	"go.uber.org/zap"
 )
 
+var l *zap.Logger
+
 // LogRequest is a gin middleware that logs useful informations on each request as they come.
-func LogRequest(l *zap.Logger) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		start := time.Now()
-		// some evil middlewares modify this values
-		path := c.Request.URL.Path
-		query := c.Request.URL.RawQuery
+func LogRequest(logger *zap.Logger) gin.HandlerFunc {
+	l = logger
+	return log
+}
 
-		l.Info(path,
-			zap.Int("status", c.Writer.Status()),
-			zap.String("method", c.Request.Method),
-			zap.String("path", path),
-			zap.String("query", query),
-			zap.String("ip", c.ClientIP()),
-			zap.String("user-agent", c.Request.UserAgent()),
-			zap.String("time", start.Format(time.RFC3339)),
-		)
+func log(c *gin.Context) {
+	start := time.Now()
+	// some evil middlewares modify this values
+	path := c.Request.URL.Path
+	query := c.Request.URL.RawQuery
 
-		c.Next()
-	}
+	l.Info(path,
+		zap.Int("status", c.Writer.Status()),
+		zap.String("method", c.Request.Method),
+		zap.String("path", path),
+		zap.String("query", query),
+		zap.String("ip", c.ClientIP()),
+		zap.String("user-agent", c.Request.UserAgent()),
+		zap.String("time", start.Format(time.RFC3339)),
+	)
+
+	c.Next()
 }
