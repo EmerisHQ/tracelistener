2b321c729ae0ed9dc3f3aa318e7f674b2bfc2996
PrathyushaLakkireddy <prathyusha@vitwit.com>
2021-11-22 14:30:54 +0530

    update delegation tests

--- tracelistener/gaia_processor/delegation_test.go
+++ tracelistener/gaia_processor/delegation_test.go
@@ -5,7 +5,6 @@ import (
 
 	sdk_types "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/staking/types"
-	gaia "github.com/cosmos/gaia/v4/app"
 	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
 
@@ -17,28 +16,29 @@ import (
 func TestDelegationProcess(t *testing.T) {
 	d := delegationsProcessor{}
 
-	DataProcessor, _ := New(zap.NewNop().Sugar(), &config.Config{})
+	DataProcessor, err := New(zap.NewNop().Sugar(), &config.Config{})
+	require.NoError(t, err)
 
 	gp := DataProcessor.(*Processor)
 	require.NotNil(t, gp)
 	p.cdc = gp.cdc
 
 	tests := []struct {
-		name       string
-		delegation types.Delegation
-		newMessage tracelistener.TraceOperation
-		wantErr    bool
+		name        string
+		delegation  types.Delegation
+		newMessage  tracelistener.TraceOperation
+		wantErr     bool
+		expectedLen int
 	}{
 		{
 			"Delete operation of delegation - no error",
 			types.Delegation{},
 			tracelistener.TraceOperation{
-				Operation:   string(tracelistener.DeleteOp),
-				Key:         []byte("QXRkbFY4cUQ2bzZKMnNoc2o5YWNwSSs5T3BkL2U1dVRxWklpN05LNWkzeTk="),
-				Value:       []byte("100stake"),
-				BlockHeight: 0,
+				Operation: string(tracelistener.DeleteOp),
+				Key:       []byte("QXRkbFY4cUQ2bzZKMnNoc2o5YWNwSSs5T3BkL2U1dVRxWklpN05LNWkzeTk="),
 			},
 			false,
+			1,
 		},
 		{
 			"Write new delegation - no error",
@@ -54,6 +54,7 @@ func TestDelegationProcess(t *testing.T) {
 				TxHash:      "A5CF62609D62ADDE56816681B6191F5F0252D2800FC2C312EB91D962AB7A97CB",
 			},
 			false,
+			1,
 		},
 		{
 			"Invalid addresses - error",
@@ -69,29 +70,50 @@ func TestDelegationProcess(t *testing.T) {
 				TxHash:      "A5CF62609D62ADDE56816681B6191F5F0252D2800FC2C312EB91D962AB7A97CB",
 			},
 			true,
+			0,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-
 			d.insertHeightCache = map[delegationCacheEntry]models.DelegationRow{}
 			d.deleteHeightCache = map[delegationCacheEntry]models.DelegationRow{}
 			d.l = zap.NewNop().Sugar()
 
-			cdc, _ := gaia.MakeCodecs()
-
-			delValue, _ := cdc.MarshalBinaryBare(&tt.delegation)
-			tt.newMessage.Value = delValue
-
-			err := d.Process(tt.newMessage)
-			if tt.wantErr {
-				require.Error(t, err)
-				return
-			}
-
+			delValue, err := p.cdc.MarshalBinaryBare(&tt.delegation)
 			require.NoError(t, err)
 
+			tt.newMessage.Value = delValue
+
+			err = d.Process(tt.newMessage)
+			if tt.wantErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+
+			if tt.newMessage.Operation == tracelistener.DeleteOp.String() {
+				require.Len(t, d.deleteHeightCache, tt.expectedLen)
+
+				for k, _ := range d.deleteHeightCache {
+					row := d.deleteHeightCache[delegationCacheEntry{delegator: k.delegator, validator: k.validator}]
+					require.NotNil(t, row)
+
+					return
+				}
+			} else {
+				require.Len(t, d.insertHeightCache, tt.expectedLen)
+
+				for k, _ := range d.insertHeightCache {
+					row := d.insertHeightCache[delegationCacheEntry{delegator: k.delegator, validator: k.validator}]
+					require.NotNil(t, row)
+
+					amount := row.Amount
+					require.Equal(t, tt.delegation.Shares.String(), amount)
+
+					return
+				}
+			}
 		})
 	}
 }
