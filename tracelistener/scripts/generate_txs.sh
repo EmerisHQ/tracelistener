fb178c34dffc1f561881ee036500ed619027f941
PrathyushaLakkireddy <prathyusha@vitwit.com>
2021-12-07 13:01:57 +0530

    Refactored script to setup multichain

--- tracelistener/scripts/generate_txs.sh
+++ tracelistener/scripts/generate_txs.sh
@@ -7,7 +7,7 @@ display_usage() {
     exit 1
 }
 
-export DAEMON_HOME=~/.gaia
+export DAEMON_HOME=~/.gaia_test
 export CHAINID=test
 export DENOM=stake
 export DAEMON=gaiad
@@ -19,29 +19,13 @@ fi
 
 echo
 
-# read no.of nodes
-NODES=$1
-NODES=$1
-if [ -z $NODES ]
-then
-    NODES=2
-fi
+IP=127.0.0.1
 
-echo
-
-echo "** Number of nodes mentioned : $NODES **"
-
-IP="$(dig +short myip.opendns.com @resolver1.opendns.com)"
-echo "Public IP address: ${IP}"
-
-if [ -z $IP ]
-then
-    IP=127.0.0.1
-fi
+CHAINS=2
 
 echo "--------- Delegation tx -----------"
 
-for (( a=1; a<$NODES; a++ ))
+for (( a=1; a<=$CHAINS; a++ ))
 do
     DIFF=`expr $a - 1`
     INC=`expr $DIFF \* 2`
@@ -49,13 +33,13 @@ do
     RPC="http://${IP}:${PORT}"
     echo "NODE :: $RPC"
 
-    TONODE=`expr 1 + $a`
+    TONODE=$a
     echo "To node number : $TONODE"
 
-    validator=$("${DAEMON}" keys show "validator${TONODE}" --bech val --keyring-backend test --home $DAEMON_HOME-${TONODE} --output json)
+    validator=$("${DAEMON}" keys show validator${TONODE} --bech val --keyring-backend test --home $DAEMON_HOME-${a} --output json)
     VALADDRESS=$(echo "${validator}" | jq -r '.address')
 
-    FROMKEY="validator${a}"
+    FROMKEY=validator${a}
     TO=$VALADDRESS
     TOKEY="validator${TONODE}"
 
@@ -64,7 +48,7 @@ do
     echo "Iteration no $a and values of from : $FROMKEY to : $TO"
     echo "--------- Delegation from $FROMKEY to $TO-----------"
 
-    dTx=$("${DAEMON}" tx staking delegate "${TO}" 10000"${DENOM}" --from $FROMKEY --fees 1000"${DENOM}" --chain-id "${CHAINID}" --keyring-backend test --home $DAEMON_HOME-${a} --node $RPC --output json -y)
+    dTx=$("${DAEMON}" tx staking delegate "${TO}" 10000"${DENOM}" --from $FROMKEY --fees 1000"${DENOM}" --keyring-backend test --home $DAEMON_HOME-${a} --node $RPC --chain-id $CHAINID-$a --output json -y)
     
     sleep 6s
 
@@ -72,7 +56,7 @@ do
     echo "** TX HASH :: $dtxHash **"
 
     # query the txhash and check the code
-    txResult=$("${DAEMON}" q tx "${dtxHash}" --node $RPC --output json)
+    txResult=$("${DAEMON}" q tx "${dtxHash}" --node $RPC --chain-id $CHAINID-$a --output json)
     dTxCode=$(echo "${txResult}"| jq -r '.code')
 
     echo "Code is : $dTxCode"
@@ -84,32 +68,24 @@ do
         echo "**** Delegation from $FROMKEY to $TOKEY has FAILED!!!!   txHash is : $dtxHash and REASON : $(echo "${dTx}" | jq '.raw_log')***"
     fi
     echo
-done
 
-echo
 
-echo "--------- Unbond txs -----------"
+    echo
 
-for (( a=1; a<$NODES; a++ ))
-do
-    DIFF=`expr $a - 1`
-    INC=`expr $DIFF \* 2`
-    PORT=`expr 16657 + $INC` #get ports
-    RPC="http://${IP}:${PORT}"
-    echo "NODE :: $RPC"
+    echo "--------- Unbond txs -----------"
 
-    validator=$("${DAEMON}" keys show "validator${a}" --bech val --keyring-backend test --home $DAEMON_HOME-${a} --output json)
+    validator=$("${DAEMON}" keys show validator${a} --bech val --keyring-backend test --home $DAEMON_HOME-${a} --output json)
     VALADDRESS=$(echo "${validator}" | jq -r '.address')
 
     FROM=${VALADDRESS}
-    FROMKEY="validator${a}"
+    FROMKEY=validator${a}
     echo "** validator address :: $FROM and From key :: $FROMKEY **"
 
     # Print the value
     echo "Iteration no $a and values of from : $FROM and fromKey : $FROMKEY"
     echo "--------- Running unbond tx command of $FROM and key : $FROMKEY------------"
 
-    ubTx=$("${DAEMON}" tx staking unbond "${FROM}" 10000"${DENOM}" --from "${FROMKEY}" --fees 1000"${DENOM}" --chain-id "${CHAINID}" --keyring-backend test --home $DAEMON_HOME-${a} --node $RPC --output json -y)
+    ubTx=$("${DAEMON}" tx staking unbond "${FROM}" 10000"${DENOM}" --from "${FROMKEY}" --fees 1000"${DENOM}" --chain-id $CHAINID-$a --keyring-backend test --home $DAEMON_HOME-${a} --node $RPC --output json -y)
     
     sleep 6s
     
@@ -117,7 +93,7 @@ do
     echo "** TX HASH :: $ubtxHash **"
 
     # query the txhash and check the code
-    txResult=$("${DAEMON}" q tx "${ubtxHash}" --node $RPC --output json)
+    txResult=$("${DAEMON}" q tx "${ubtxHash}" --node $RPC --chain-id $CHAINID-$a --output json)
     ubTxCode=$(echo "${txResult}"| jq -r '.code')
 
     echo "Code is : $ubTxCode"
@@ -129,26 +105,17 @@ do
         echo "**** Unbond tx ( of $FROM and key $FROMKEY ) FAILED!!!!   txHash is : $ubtxHash  and REASON : $(echo "${ubTx}" | jq '.raw_log')  ***"
     fi
     echo
-done
 
-echo
+    echo
 
-echo "--------- Send txn -----------"
+    echo "--------- Send txn -----------"
 
-for (( a=1; a<$NODES; a++ ))
-do
-    DIFF=`expr $a - 1`
-    INC=`expr $DIFF \* 2`
-    PORT=`expr 16657 + $INC` #get ports
-    RPC="http://${IP}:${PORT}"
-    echo "NODE :: $RPC"
-
-    TONODE=`expr 1 + $a`
+    TONODE=$a
     echo "To node number : $TONODE"
 
-    VALADDRESS=$("${DAEMON}" keys show "validator${TONODE}" -a --keyring-backend test --home $DAEMON_HOME-${TONODE})
+    VALADDRESS=$("${DAEMON}" keys show validator${TONODE} -a --keyring-backend test --home $DAEMON_HOME-${TONODE})
 
-    FROMKEY="validator${a}"
+    FROMKEY=validator${a}
     TO=$VALADDRESS
     TOKEY="validator${TONODE}"
 
@@ -157,7 +124,7 @@ do
     echo "Iteration no $a and values of from : $FROMKEY to : $TO"
     echo "--------- Send tx from $FROMKEY to $TO-----------"
 
-    sendTx=$("${DAEMON}" tx bank send $FROMKEY "${TO}" 10000"${DENOM}" --fees 1000"${DENOM}" --chain-id "${CHAINID}" --keyring-backend test --home $DAEMON_HOME-${a} --node $RPC --output json -y)
+    sendTx=$("${DAEMON}" tx bank send $FROMKEY "${TO}" 10000"${DENOM}" --fees 1000"${DENOM}" --chain-id $CHAINID-$a --keyring-backend test --home $DAEMON_HOME-${a} --node $RPC --output json -y)
     
     sleep 6s
 
@@ -165,7 +132,7 @@ do
     echo "** TX HASH :: $sendTxHash **"
 
     # query the txhash and check the code
-    txResult=$("${DAEMON}" q tx "${sendTxHash}" --node $RPC --output json)
+    txResult=$("${DAEMON}" q tx "${sendTxHash}" --node $RPC --chain-id $CHAINID-$a --output json)
     sendTxCode=$(echo "${txResult}"| jq -r '.code')
 
     echo "Code is : $sendTxCode"
@@ -178,29 +145,28 @@ do
     fi
     echo
 
+    echo "----------create validator-----------"
+
+    FROMKEY=validator${a}
+    RPC="http://${IP}:16657"
+
+    createValidator=$("$DAEMON" tx staking create-validator --amount 4000000000000"${DENOM}" --commission-max-change-rate 0.1 --commission-max-rate 0.2 --commission-rate 0.1 --from $FROMKEY --min-self-delegation 1 --moniker moniker-$a --pubkey $("${DAEMON}" tendermint show-validator --home $DAEMON_HOME-$a) --chain-id $CHAINID-$a --home $DAEMON_HOME-$a --node $RPC --keyring-backend test --output json -y)
+    sleep 6s
+
+    valTxHash=$(echo "${createValidator}" | jq -r '.txhash')
+    echo "** TX HASH :: $valTxHash **"
+
+    # query the txhash and check the code
+    txResult=$("${DAEMON}" q tx "${valTxHash}" --node $RPC --chain-id $CHAINID-$a --output json)
+    valTxCode=$(echo "${txResult}"| jq -r '.code')
+
+    echo "Code is : $valTxCode"
+    echo
+    if [ "$valTxCode" -eq 0 ];
+    then
+        echo "**** Create validator tx is SUCCESSFULL!!  txHash is : $valTxHash ****"
+    else 
+        echo "**** Create validator of validator1 has FAILED!!!!   txHash is : $valTxHash and REASON : $(echo "${valTxHash}" | jq '.raw_log')***"
+    fi
+    echo
 done
-
-echo "----------create validator-----------"
-
-FROMKEY="validator1"
-RPC="http://${IP}:16657"
-
-createValidator=$("$DAEMON" tx staking create-validator --amount 4000000000000"${DENOM}" --commission-max-change-rate 0.1 --commission-max-rate 0.2 --commission-rate 0.1 --from "${FROMKEY}" --min-self-delegation 1 --moniker testmoniker --pubkey $("${DAEMON}" tendermint show-validator --home $DAEMON_HOME-1) --chain-id "${CHAINID}" --home $DAEMON_HOME-1 --node $RPC --keyring-backend test --output json -y)
-sleep 6s
-
-valTxHash=$(echo "${createValidator}" | jq -r '.txhash')
-echo "** TX HASH :: $valTxHash **"
-
-# query the txhash and check the code
-txResult=$("${DAEMON}" q tx "${valTxHash}" --node $RPC --output json)
-valTxCode=$(echo "${txResult}"| jq -r '.code')
-
-echo "Code is : $valTxCode"
-echo
-if [ "$valTxCode" -eq 0 ];
-then
-echo "**** Create validator tx is SUCCESSFULL!!  txHash is : $valTxHash ****"
-else 
-    echo "**** Create validator of validator1 has FAILED!!!!   txHash is : $valTxHash and REASON : $(echo "${valTxHash}" | jq '.raw_log')***"
-fi
-echo
---
+++ tracelistener/scripts/multichain_setup_script.sh
@@ -0,0 +1,146 @@
+#/bin/sh
+
+cd $HOME
+
+export DAEMON_HOME=~/.gaia_test
+export CHAINID=test
+export DENOM=stake
+export GH_URL=https://github.com/cosmos/gaia
+export CHAIN_VERSION=v5.0.4 
+export DAEMON=gaiad
+export TRACELISTENER_URL=github.com/allinbits/tracelistener
+
+command_exists () {
+    type "$1" &> /dev/null ;
+}
+
+cd $HOME
+
+if command_exists go ; then
+    echo "Golang is already installed"
+else
+  echo "Install dependencies"
+  sudo apt update
+  sudo apt-get -y upgrade
+  sudo apt install build-essential jq -y
+
+  wget https://dl.google.com/go/go1.17.3.linux-amd64.tar.gz
+  tar -xvf go1.17.3.linux-amd64.tar.gz
+  sudo mv go /usr/local
+  rm go1.17.3.linux-amd64.tar.gz
+
+  echo "------ Update bashrc ---------------"
+  export GOPATH=$HOME/go
+  export GOROOT=/usr/local/go
+  export GOBIN=$GOPATH/bin
+  export PATH=$PATH:/usr/local/go/bin:$GOBIN
+  echo "" >> ~/.bashrc
+  echo 'export GOPATH=$HOME/go' >> ~/.bashrc
+  echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
+  echo 'export GOBIN=$GOPATH/bin' >> ~/.bashrc
+  echo 'export PATH=$PATH:/usr/local/go/bin:$GOBIN' >> ~/.bashrc
+
+  source ~/.bashrc
+
+  mkdir -p "$GOBIN"
+  mkdir -p $GOPATH/src/github.com
+
+  go version
+fi
+
+echo "--------- Install $DAEMON ---------"
+git clone $GH_URL && cd $(basename $_ .git)
+git fetch && git checkout $CHAIN_VERSION
+make install
+
+cd $HOME
+
+# check version
+$DAEMON version --long
+
+for (( c=1; c<=2; c++ ))
+do
+    export DAEMON_HOME_$c=$DAEMON_HOME-$c
+    echo "Deamon path :: $DAEMON_HOME-$c"
+
+    $DAEMON unsafe-reset-all  --home $DAEMON_HOME-$c
+    echo "****** here command $DAEMON unsafe-reset-all  --home $DAEMON_HOME-$c ******"
+
+    # remove daemon home directories if already exists
+    rm -rf $DAEMON_HOME-$c
+
+    echo "-----Create daemon home directories------"
+
+   
+    echo "****** create dir :: $DAEMON_HOME-$c ********"
+    mkdir -p "$DAEMON_HOME-$c"
+    
+
+    echo "--------Start initializing the chain ($CHAINID-$c)---------"
+
+   
+    echo "-------Init chain ${a}--------"
+    echo "Deamon home :: $DAEMON_HOME-$c"
+    $DAEMON init --chain-id $CHAINID-$c $DAEMON_HOME-${c} --home $DAEMON_HOME-${c}
+    
+
+    # add validators
+    echo "---------Creating keys-------------"
+
+    $DAEMON keys add "validator${c}" --keyring-backend test --home $DAEMON_HOME-${c}
+
+    echo "----------Genesis creation---------"
+       
+    $DAEMON --home $DAEMON_HOME-$c add-genesis-account $($DAEMON keys show validator$c -a --home $DAEMON_HOME-$c --keyring-backend test) 1000000000000$DENOM
+
+    echo "--------Gentx--------"
+
+    $DAEMON gentx validator$c 90000000000$DENOM --chain-id $CHAINID-$c  --keyring-backend test --home $DAEMON_HOME-$c
+
+    echo "----------collect-gentxs------------"
+
+    $DAEMON collect-gentxs --home $DAEMON_HOME-$c
+
+    echo "---------Updating $DAEMON_HOME-$c genesis.json ------------"
+
+    sed -i "s/172800000000000/600000000000/g" $DAEMON_HOME-$c/config/genesis.json
+    sed -i "s/172800s/600s/g" $DAEMON_HOME-$c/config/genesis.json
+    sed -i "s/stake/$DENOM/g" $DAEMON_HOME-$c/config/genesis.json
+
+
+    DIFF=`expr $c - 1`
+    INC=`expr $DIFF \* 2`
+
+    RPC=`expr 16657 + $INC` #increment rpc ports
+    LADDR=`expr 16656 + $INC` #laddr ports
+    GRPC=`expr 9090 + $INC` # grpc poprt
+    WGRPC=`expr 9091 + $INC` # web grpc port
+
+    echo "PORTS OF RPC :: $RPC , LADDR :: $LADDR , GRPC :: $GRPC , WGRPC :: $WGRPC"
+
+    echo "----------Updating $DAEMON_HOME-$c chain config-----------"
+
+    sed -i 's#tcp://127.0.0.1:26657#tcp://0.0.0.0:'${RPC}'#g' $DAEMON_HOME-$c/config/config.toml
+    sed -i 's#tcp://0.0.0.0:26656#tcp://0.0.0.0:'${LADDR}'#g' $DAEMON_HOME-$c/config/config.toml
+    #sed -i '/persistent_peers =/c\persistent_peers = "'"$PERSISTENT_PEERS"'"' $DAEMON_HOME-$c/config/config.toml
+    sed -i '/allow_duplicate_ip =/c\allow_duplicate_ip = true' $DAEMON_HOME-$c/config/config.toml
+    sed -i '/pprof_laddr =/c\# pprof_laddr = "localhost:6060"' $DAEMON_HOME-$c/config/config.toml
+
+    sed -i 's#0.0.0.0:9090#0.0.0.0:'${GRPC}'#g' $DAEMON_HOME-$c/config/app.toml
+    sed -i 's#0.0.0.0:9091#0.0.0.0:'${WGRPC}'#g' $DAEMON_HOME-$c/config/app.toml
+
+    sed -i '/max_num_inbound_peers =/c\max_num_inbound_peers = 140' $DAEMON_HOME-$c/config/config.toml
+    sed -i '/max_num_outbound_peers =/c\max_num_outbound_peers = 110' $DAEMON_HOME-$c/config/config.toml
+
+    #start chains
+   
+    $DAEMON start --home $DAEMON_HOME-$c </dev/null &>/dev/null &
+
+    sleep 4s
+
+    echo "Checking $DAEMON_HOME-${c} chain status"
+
+    $DAEMON status --node tcp://localhost:${RPC}
+
+    echo
+done
