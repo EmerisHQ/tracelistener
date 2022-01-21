9027490f4074feadfad49683c016b1cfd7785a0c
Gianguido Sorà <me@gsora.xyz>
2022-01-21 16:38:19 +0100

    chore(utils): vendor bech32 decode and convert function from cosmos-sdk

--- tracelistener/processor/utils.go
+++ tracelistener/processor/utils.go
@@ -2,15 +2,31 @@ package processor
 
 import (
 	"encoding/hex"
+	"fmt"
 
-	"github.com/cosmos/cosmos-sdk/types/bech32"
+	"github.com/cosmos/btcutil/bech32"
 )
 
 func b32Hex(s string) (string, error) {
-	_, b, err := bech32.DecodeAndConvert(s)
+	_, b, err := decodeAndConvert(s)
 	if err != nil {
 		return "", err
 	}
 
 	return hex.EncodeToString(b), nil
+}
+
+// vendored from cosmos-sdk, no need to bring it as a dep here just because of this
+func decodeAndConvert(bech string) (string, []byte, error) {
+	hrp, data, err := bech32.Decode(bech, 1023)
+	if err != nil {
+		return "", nil, fmt.Errorf("decoding bech32 failed: %w", err)
+	}
+
+	converted, err := bech32.ConvertBits(data, 5, 8, false)
+	if err != nil {
+		return "", nil, fmt.Errorf("decoding bech32 failed: %w", err)
+	}
+
+	return hrp, converted, nil
 }
