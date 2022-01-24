335ea3a233754f792354024bb6a0e987259ef920
Gianguido Sorà <me@gsora.xyz>
2022-01-24 11:28:56 +0100

    chore(datamarshaler): add comment on codec func file

--- tracelistener/processor/datamarshaler/codec_func_v42.go
+++ tracelistener/processor/datamarshaler/codec_func_v42.go
@@ -7,6 +7,9 @@ import (
 	"github.com/gogo/protobuf/proto"
 )
 
+// This file contains implementation for Cosmos SDK v0.42.x for marshaling interfaces.
+// Those functions are often used in impl_test_handler.go.
+
 func marshalIfaceOrPanic(p proto.Message) []byte {
 	data, err := getCodec().MarshalInterface(p)
 	if err != nil {
