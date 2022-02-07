91891ead93c198407fda49374c56e586d3b4d047
Gianguido Sorà <me@gsora.xyz>
2022-02-07 11:27:20 +0100

    chore(utils): manually ignore deadcode and unused lints on utils func
    false positives 100%

--- tracelistener/processor/utils.go
+++ tracelistener/processor/utils.go
@@ -7,6 +7,8 @@ import (
 	"github.com/cosmos/btcutil/bech32"
 )
 
+// for some weird reason golangci-lint hates us, and marks this func as unused, weird
+//nolint:unused,deadcode
 func b32Hex(s string) (string, error) {
 	_, b, err := decodeAndConvert(s)
 	if err != nil {
@@ -17,6 +19,8 @@ func b32Hex(s string) (string, error) {
 }
 
 // vendored from cosmos-sdk, no need to bring it as a dep here just because of this
+// for some weird reason golangci-lint hates us, and marks this func as unused, weird
+//nolint:unused,deadcode
 func decodeAndConvert(bech string) (string, []byte, error) {
 	hrp, data, err := bech32.Decode(bech, 1023)
 	if err != nil {
