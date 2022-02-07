f6c2a0300e5330a14e110c295d13359fa32ee261
Gianguido Sorà <me@gsora.xyz>
2022-02-07 11:43:48 +0100

    chore(processor): move prod-unused func to tests

--- tracelistener/processor/utils_test.go
+++ tracelistener/processor/utils_test.go
@@ -7,8 +7,6 @@ import (
 	"github.com/cosmos/btcutil/bech32"
 )
 
-// for some weird reason golangci-lint hates us, and marks this func as unused, weird
-//nolint:unused,deadcode
 func b32Hex(s string) (string, error) {
 	_, b, err := decodeAndConvert(s)
 	if err != nil {
@@ -19,8 +17,6 @@ func b32Hex(s string) (string, error) {
 }
 
 // vendored from cosmos-sdk, no need to bring it as a dep here just because of this
-// for some weird reason golangci-lint hates us, and marks this func as unused, weird
-//nolint:unused,deadcode
 func decodeAndConvert(bech string) (string, []byte, error) {
 	hrp, data, err := bech32.Decode(bech, 1023)
 	if err != nil {
