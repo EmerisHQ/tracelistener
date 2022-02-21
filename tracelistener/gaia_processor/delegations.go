da5218de3ac955bed335bfe8cbdc52519be7547c
tamjidahmed <mail.to.tamjid@gmail.com>
2022-02-21 11:47:42 +0600

    len prefixed delegation key split

--- tracelistener/gaia_processor/delegations.go
+++ tracelistener/gaia_processor/delegations.go
@@ -2,7 +2,6 @@ package gaia_processor
 
 import (
 	"bytes"
-	"encoding/hex"
 	"fmt"
 
 	"github.com/cosmos/cosmos-sdk/x/staking/types"
@@ -71,26 +70,18 @@ func (b *delegationsProcessor) OwnsKey(key []byte) bool {
 
 func (b *delegationsProcessor) Process(data tracelistener.TraceOperation) error {
 	if data.Operation == tracelistener.DeleteOp.String() {
-		if len(data.Key) < 3 { // At least Prefix + lenDelegator + lenValidator
-			return fmt.Errorf("malformed key: %v", data.Key)
+		_, delegator, validator, err := tracelistener.SplitDelegationKey(data.Key)
+		if err != nil {
+			return err
 		}
-		addresses := data.Key[1:] // Strip the prefix byte.
-		delegatorAddrLength := addresses[0]
-		addresses = addresses[1:] // Strip the address byte.
-		delegatorAddr := hex.EncodeToString(addresses[0:delegatorAddrLength])
-
-		addresses = addresses[delegatorAddrLength:] // Strip the delegator address portion.
-		addresses = addresses[1:]                   // Strip the address length byte.
-
-		validatorAddr := hex.EncodeToString(addresses[0:])
-		b.l.Debugw("new delegation delete", "delegatorAddr", delegatorAddr, "validatorAddr", validatorAddr)
+		b.l.Debugw("new delegation delete", "delegator", delegator, "validator", validator)
 
 		b.deleteHeightCache[delegationCacheEntry{
-			validator: validatorAddr,
-			delegator: delegatorAddr,
+			delegator: delegator,
+			validator: validator,
 		}] = models.DelegationRow{
-			Delegator: delegatorAddr,
-			Validator: validatorAddr,
+			Delegator: delegator,
+			Validator: validator,
 		}
 
 		return nil
--- tracelistener/keys.go
+++ tracelistener/keys.go
@@ -5,27 +5,54 @@ import (
 	"fmt"
 )
 
-// SplitDelegatorValidatorAddress given a key split it into delegator and
+// SplitDelegationKey given a key, split it into prefix, delegator and
 // validator address.
-// param <key> is a list of byte where the key is a concatenation of 5 parts
-// 1. Prefix                       1 Byte
-// 2. Delegator Address length     1 Byte
-// 3. Delegator Address            From 2
-// 4. Validator Address Length +   1 Byte
-// 5. Validator Address            From 4
-func SplitDelegatorValidatorAddress(key []byte) (string, string, error) {
-	if len(key) < 3 { // At least 1, 2, 4 must be present
-		return "", "", fmt.Errorf("malformed key: %v", key)
+// param <key> is a list of byte. The key is a concatenation of 5 parts,
+// 1. Prefix                    length: 1 Byte
+// 2. Delegator Address Len		length: 1 Byte
+// 3. Delegator Address      	length: From 2
+// 4. Validator Address Len   	length: 1 Byte
+// 5. Validator Address         length: From 4
+// key : <prefix><del-addr-len><del-addr><val-addr-len><val-addr>
+// Len	    1	    0 - 255       <-         0 - 255      <-
+//
+// Note: Address len 0 does not make sense, but since in the SDK it's "possible" to
+// have 0 len address for delegator/validator, we also consider empty address valid.
+func SplitDelegationKey(key []byte) (byte, string, string, error) {
+	// At-least: 3 bytes   -> 1, 2, 4 must be present. 1 byte each.
+	// At-max  : 513 bytes -> 3 bytes from (1, 2, 4) + 510 bytes from (3, 5).
+	if len(key) < 3 || len(key) > (1+1+255+1+255) {
+		return 0, "", "", fmt.Errorf("malformed key: length %d not in range", len(key))
+	}
+	prefix, addresses := key[0], key[1:]                 // Strip the prefix byte.
+	delAddrLen, addresses := addresses[0], addresses[1:] // Strip the delegator length byte.
+	if delAddrLen > 255 {
+		return 0, "", "", fmt.Errorf("malformed key: delegator address length out of range %d", delAddrLen)
 	}
 
-	addresses := key[1:] // Strip the prefix byte.
-	delegatorAddrLength := addresses[0]
-	addresses = addresses[1:] // Strip the address byte.
-	delegatorAddr := hex.EncodeToString(addresses[0:delegatorAddrLength])
+	// Check if we have enough bytes for the address of delegator + at least 1 byte for validator address length
+	if len(addresses) < int(delAddrLen)+1 {
+		return 0, "", "", fmt.Errorf(
+			"malformed key: delegator key length not sufficient. want atlease: %d got: %d",
+			delAddrLen+1,
+			len(addresses),
+		)
+	}
+	delAddr := hex.EncodeToString(addresses[0:delAddrLen])
 
-	addresses = addresses[delegatorAddrLength:] // Strip the delegator address portion.
-	addresses = addresses[1:]                   // Strip the address length byte.
-
-	validatorAddr := hex.EncodeToString(addresses[0:])
-	return delegatorAddr, validatorAddr, nil
+	addresses = addresses[delAddrLen:]                   // Strip the delegator address.
+	valAddrLen, addresses := addresses[0], addresses[1:] // Strip the address length byte.
+	if valAddrLen > 255 {
+		return 0, "", "", fmt.Errorf("malformed key: validator address length out of range %d", valAddrLen)
+	}
+	// Check if we have exact number of bytes for the address of validator.
+	if len(addresses) != int(valAddrLen) {
+		return 0, "", "", fmt.Errorf(
+			"malformed key: validator. want: %d got: %d",
+			valAddrLen,
+			len(addresses),
+		)
+	}
+	valAddr := hex.EncodeToString(addresses)
+	return prefix, delAddr, valAddr, nil
 }
