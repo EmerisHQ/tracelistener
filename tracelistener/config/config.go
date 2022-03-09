950a999d3e1db48ce31da72944b1b540a7c6390c
Antonio Pitasi <antonio@pitasi.dev>
2022-03-09 18:21:30 +0100

    fix: add EnableCpuProfiling env variable instead of using Debug
    This was causing issues on staging environment where we want debug logs
    but we don't want cpu profiling because it impacts performances and we
    also don't have access to create a profile files.

--- tracelistener/config/config.go
+++ tracelistener/config/config.go
@@ -14,6 +14,7 @@ type Config struct {
 	LogPath               string
 	Debug                 bool
 	JSONLogs              bool
+	EnableCpuProfiling    bool
 
 	// Processors configs
 	Processor ProcessorConfig
