60a0c39731081af139a417dbe6f6d3de9afbecfc
Gianguido Sorà <me@gsora.xyz>
2022-03-14 14:47:08 +0100

    chore(tracelistener): cleanup trace.go

--- tracelistener/trace.go
+++ tracelistener/trace.go
@@ -4,29 +4,6 @@ import (
 	"fmt"
 )
 
-const (
-	metadataBlockHeight = "blockHeight"
-	metadataTxHash      = "txHash"
-)
-
-// var toiPool = sync.Pool{
-// 	New: func() interface{} {
-// 		return &traceOperationInter{}
-// 	},
-// }
-
-// type TraceOperation struct {
-// 	Operation   string `json:"operation"`
-// 	Key         []byte `json:"key"`
-// 	Value       []byte `json:"value"`
-// 	BlockHeight uint64 `json:"block_height"`
-// 	TxHash      string `json:"tx_hash"`
-
-// 	// SuggestedProcessor signals to the trace processor that
-// 	// what SDK module this trace comes from.
-// 	SuggestedProcessor SDKModuleName
-// }
-
 func (to *TraceOperation) Copy() TraceOperation {
 	ret := TraceOperation{}
 	ret = *to
@@ -61,30 +38,3 @@ type TraceOperation struct {
 	// what SDK module this trace comes from.
 	SuggestedProcessor SDKModuleName
 }
-
-// func (toi *traceOperationInter) Reset() {
-// 	toi.Operation = ""
-// 	toi.Key = toi.Key[:0]
-// 	toi.Value = toi.Value[:0]
-// 	toi.Metadata.BlockHeight = 0
-// 	toi.Metadata.TxHash = ""
-// }
-
-// func (t *TraceOperation) UnmarshalJSON(bytes []byte) error {
-// 	toi := toiPool.Get().(*traceOperationInter)
-// 	toi.Reset()
-
-// 	if err := json.Unmarshal(bytes, &toi); err != nil {
-// 		return err
-// 	}
-
-// 	t.BlockHeight = toi.Metadata.BlockHeight
-// 	t.TxHash = toi.Metadata.TxHash
-// 	t.Operation = toi.Operation
-// 	t.Key = toi.Key
-// 	t.Value = toi.Value
-
-// 	toiPool.Put(toi)
-
-// 	return nil
-// }
