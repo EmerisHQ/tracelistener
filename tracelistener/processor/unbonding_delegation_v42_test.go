59056b3800c0c48a628ae404852a3a4ae0ded0d3
Gianguido Sorà <me@gsora.xyz>
2022-03-14 14:32:13 +0100

    chore(tracelistener,processor): fix v42 tests

--- tracelistener/processor/unbonding_delegation_v42_test.go
+++ tracelistener/processor/unbonding_delegation_v42_test.go
@@ -22,10 +22,12 @@ func versionSpecificUnbondingDelegationsProcessTests() []unbondingDelegationsPro
 				Validator: "cosmosvaloper19xawgvgn887e9gef5vkzkemwh33mtgwa6haa7s",
 			},
 			tracelistener.TraceOperation{
-				Operation:   string(tracelistener.DeleteOp),
-				Key:         []byte("QXRkbFY4cUQ2bzZKMnNoc2o5YWNwSSs5T3BkL2U1dVRxWklpN05LNWkzeTk="),
-				Value:       []byte("Ci1jb3Ntb3MxeHJubmVyOXM3ODM0NDZ5ejNoaHNocHI1ZnB6Nnd6Y3drdnd2NWoSNGNvc21vc3ZhbG9wZXIxOXhhd2d2Z244ODdlOWdlZjV2a3prZW13aDMzbXRnd2E2aGFhN3MaHAiYIBILCICSuMOY/v///wEaBDEwMDAiBDExMDA="),
-				BlockHeight: 0,
+				Operation: string(tracelistener.DeleteOp),
+				Key:       []byte("QXRkbFY4cUQ2bzZKMnNoc2o5YWNwSSs5T3BkL2U1dVRxWklpN05LNWkzeTk="),
+				Value:     []byte("Ci1jb3Ntb3MxeHJubmVyOXM3ODM0NDZ5ejNoaHNocHI1ZnB6Nnd6Y3drdnd2NWoSNGNvc21vc3ZhbG9wZXIxOXhhd2d2Z244ODdlOWdlZjV2a3prZW13aDMzbXRnd2E2aGFhN3MaHAiYIBILCICSuMOY/v///wEaBDEwMDAiBDExMDA="),
+				Metadata: tracelistener.TraceMetadata{
+					BlockHeight: 0,
+				},
 			},
 			false,
 			1,
