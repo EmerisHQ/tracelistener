80d1858b7135ba39f11814d8675b877fa30c38ad
Gianguido Sorà <me@gsora.xyz>
2022-03-15 10:28:30 +0100

    fix(tracelistener): address struct copying concern
    Also added missing godoc strings, removed `TraceOperation.String()`
    method, it wasn't needed anymore.

--- tracelistener/trace.go
+++ tracelistener/trace.go
@@ -1,14 +1,23 @@
 package tracelistener
 
-import (
-	"fmt"
-)
-
+// Copy deep-copies to to a new instances of TraceOperation,
+// useful when sending over data down the processing pipeline.
 func (to *TraceOperation) Copy() TraceOperation {
 	ret := *to
+
+	// Explicitly copy key and value slices to new
+	// slice instances to avoid aliasing.
+	ret.Key = make([]byte, len(to.Key))
+	copy(ret.Key, to.Key)
+
+	ret.Value = make([]byte, len(to.Value))
+	copy(ret.Value, to.Value)
+
 	return ret
 }
 
+// Reset resets to to an empty state.
+// Useful when storing it in a sync.Pool.
 func (to *TraceOperation) Reset() {
 	to.Operation = ""
 	to.Key = to.Key[:0]
@@ -18,15 +27,16 @@ func (to *TraceOperation) Reset() {
 	to.SuggestedProcessor = ""
 }
 
-func (to TraceOperation) String() string {
-	return fmt.Sprintf(`[%s] "%v" -> "%v"`, to.Operation, string(to.Key), string(to.Value))
-}
-
+// TraceMetadata holds circumstantial information about a trace,
+// like the block height at which it was generated, and optionally a
+// the block hash that generated it.
 type TraceMetadata struct {
 	BlockHeight uint64 `json:"blockHeight"`
 	TxHash      string `json:"txHash"`
 }
 
+// TraceOperation represents a Cosmos SDK store operation, parsed from
+// JSON lines produced by the SDK's "--trace-store" CLI flag.
 type TraceOperation struct {
 	Operation string        `json:"operation"`
 	Key       []byte        `json:"key"`
