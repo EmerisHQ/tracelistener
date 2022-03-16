b8ce73e37e8884ce6ef1a9c06e963e86f82ca511
Antonio Pitasi <antonio@pitasi.dev>
2022-03-16 09:41:23 +0100

    test(resetchain): use require instead of assert

--- cmd/resetchain/resetter_test.go
+++ cmd/resetchain/resetter_test.go
@@ -7,27 +7,27 @@ import (
 
 	"github.com/allinbits/emeris-utils/database"
 	"github.com/cockroachdb/cockroach-go/v2/testserver"
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"go.uber.org/zap"
 )
 
 var l *zap.SugaredLogger = zap.NewExample().Sugar()
 
 func getTestInstance(t *testing.T) *database.Instance {
-	assert := assert.New(t)
+	require := require.New(t)
 
 	ts, err := testserver.NewTestServer()
-	assert.NoError(err)
+	require.NoError(err)
 	t.Cleanup(ts.Stop)
 
 	url := ts.PGURL()
-	assert.NotEmpty(url)
+	require.NotEmpty(url)
 
 	db, err := database.New(url.String())
-	assert.NoError(err)
+	require.NoError(err)
 	t.Cleanup(func() {
 		err := db.Close()
-		assert.NoError(err)
+		require.NoError(err)
 	})
 
 	return db
@@ -38,21 +38,21 @@ func createTables(t *testing.T, db *database.Instance, tableNames ...string) {
 		_, err := db.DB.Exec(fmt.Sprintf(`
 			CREATE TABLE %s (id serial, chain_name text)
 		`, name))
-		assert.NoError(t, err)
+		require.NoError(t, err)
 	}
 }
 
 func addRows(t *testing.T, db *database.Instance, tableName, chainName string, rowCount int) {
 	for i := 0; i < rowCount; i++ {
 		_, err := db.DB.Exec(fmt.Sprintf("INSERT INTO %s VALUES ($1, $2)", tableName), i, chainName)
-		assert.NoError(t, err)
+		require.NoError(t, err)
 	}
 }
 
 func countRows(t *testing.T, db *database.Instance, tableName, chainName string) int {
 	var count int
 	err := db.DB.Get(&count, fmt.Sprintf("SELECT count(*) from %s WHERE chain_name = $1", tableName), chainName)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 	return count
 }
 
@@ -75,7 +75,7 @@ func TestResetTable_ChunkSize(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			assert := assert.New(t)
+			require := require.New(t)
 
 			// arrange
 			// create a random new table for each test so we know we start clean
@@ -87,17 +87,17 @@ func TestResetTable_ChunkSize(t *testing.T) {
 			addRows(t, db, tableName, "chain-b", 7)
 
 			count := countRows(t, db, tableName, "chain-a")
-			assert.Equal(13, count)
+			require.Equal(13, count)
 
 			// act
 			err := ResetTable(l, db.DB, tableName, "chain-a", tt.chunkSize)
-			assert.NoError(err)
+			require.NoError(err)
 
-			// assert
+			// require
 			count = countRows(t, db, tableName, "chain-a")
-			assert.Equal(0, count, "chain-a row not deleted")
+			require.Equal(0, count, "chain-a row not deleted")
 			countB := countRows(t, db, tableName, "chain-b")
-			assert.Equal(7, countB, "chain-b rows deleted but they were not supposed to")
+			require.Equal(7, countB, "chain-b rows deleted but they were not supposed to")
 		})
 	}
 }
@@ -105,5 +105,5 @@ func TestResetTable_ChunkSize(t *testing.T) {
 func TestResetTable_IgnoreNonExistentTables(t *testing.T) {
 	db := getTestInstance(t)
 	err := ResetTable(l, db.DB, "something_non_existent", "chain-a", 1)
-	assert.NoError(t, err)
+	require.NoError(t, err)
 }
