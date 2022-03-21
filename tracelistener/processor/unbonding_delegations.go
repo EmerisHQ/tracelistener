205e35fdf172bf4aa0af13a6055f91165e1343e6
Gianguido Sorà <me@gsora.xyz>
2022-03-21 15:46:26 +0100

    fix(processor): individally delete rows
    Instead of packaging delete statements into a slice of data, delete them individually.
    DELETE statements can't be batched like inserts.

--- tracelistener/processor/unbonding_delegations.go
+++ tracelistener/processor/unbonding_delegations.go
@@ -40,7 +40,11 @@ func (b *unbondingDelegationsProcessor) FlushCache() []tracelistener.WritebackOp
 	defer b.m.Unlock()
 
 	insert := make([]models.DatabaseEntrier, 0, len(b.insertHeightCache))
-	deleteEntries := make([]models.DatabaseEntrier, 0, len(b.deleteHeightCache))
+
+	// pre-allocate wbOp as follows:
+	// - 1 capacity unit for an eventual insert op
+	// - n capacity units for each element in deleteHeightCache
+	writebackOp := make([]tracelistener.WritebackOp, 0, 1+len(b.deleteHeightCache))
 
 	if len(b.insertHeightCache) != 0 {
 		for _, v := range b.insertHeightCache {
@@ -50,26 +54,25 @@ func (b *unbondingDelegationsProcessor) FlushCache() []tracelistener.WritebackOp
 		b.insertHeightCache = map[unbondingDelegationCacheEntry]models.UnbondingDelegationRow{}
 	}
 
-	if len(b.deleteHeightCache) == 0 && insert == nil {
+	writebackOp = append(writebackOp, tracelistener.WritebackOp{
+		DatabaseExec: insertUnbondingDelegation,
+		Data:         insert,
+	})
+
+	if len(b.deleteHeightCache) == 0 && len(insert) == 0 {
 		return nil
 	}
 
 	for _, v := range b.deleteHeightCache {
-		deleteEntries = append(deleteEntries, v)
+		writebackOp = append(writebackOp, tracelistener.WritebackOp{
+			DatabaseExec: deleteUnbondingDelegation,
+			Data:         []models.DatabaseEntrier{v},
+		})
 	}
 
 	b.deleteHeightCache = map[unbondingDelegationCacheEntry]models.UnbondingDelegationRow{}
 
-	return []tracelistener.WritebackOp{
-		{
-			DatabaseExec: insertUnbondingDelegation,
-			Data:         insert,
-		},
-		{
-			DatabaseExec: deleteUnbondingDelegation,
-			Data:         deleteEntries,
-		},
-	}
+	return writebackOp
 }
 
 func (b *unbondingDelegationsProcessor) OwnsKey(key []byte) bool {
